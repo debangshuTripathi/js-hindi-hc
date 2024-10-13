885aef9b7a73bc0f1c02acbf5c94650f633bf7ba
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-13 10:13:30 +0000

    mid done

---
+++ 01_basics/datatypes--summary.js
@@ -0,0 +1,18 @@
+// primitive --> 7 catgory
+/**
+ * string
+ * Number 
+ * boolean
+ * null
+ * undefined
+ * symbol
+ * Big int 
+ */
+
+ // reference (Non primitive)
+/**
+ * array
+ * Object 
+ * Functions
+ */
+

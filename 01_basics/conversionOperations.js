c4ee70588ac35cd7cffd01cd68e4e5e9123e746f
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-13 09:34:22 +0000

    conversion of data types done

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,73 @@
+// let score = "33"
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+
+// let score = undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // NaN
+
+// let score = null
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // 0
+
+// let score = "33abc"
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // NaN
+
+// OBSERVATION
+
+/**
+ * "33"--> 33
+ * "33abc"--->NaN
+ * "true"---> 1
+ * "false"---> 0
+ */
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let isLoggedIn = "debangshu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// OBSERVATIONS 
+
+/**
+ * 1 --> true
+ * 0 --> false
+ * "" --> false
+ * "debnagshu" --> true
+ */
+
+let number = 33
+
+let stnumber = String(number)
+console.log(stnumber);
+console.log(typeof(stnumber));
+

2446b530b365d854d64f713f9380beb5c4a2c52f
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-13 09:14:25 +0000

    done datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as a newer version
+
+//alert(3+3) // we are using node.js , not browser
+
+console.log(3+3); console.log("debangshu");
+
+let name = "DEbangshu"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// big_int 
+// string --> ""
+// boolean ---> true / false
+// null --> stand alone value 
+// undefine ---> declear but not assign any value
+// symbol --> use in react for unique ness 
+
+
+// object 
+
+console.log(typeof("hitesh")); // string 
+console.log(typeof(null));  // object 
+console.log(typeof(undefined)); // undefined
+

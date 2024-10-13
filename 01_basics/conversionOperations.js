30e27a2efbce4fe82d2bda6e1b1b62f0ccb1a078
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-13 09:52:51 +0000

    operations done

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -52,9 +52,9 @@
 // console.log(booleanIsLoggedIn);
 
 
-let isLoggedIn = "debangshu"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "debangshu"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // OBSERVATIONS 
 
@@ -65,9 +65,38 @@ console.log(booleanIsLoggedIn);
  * "debnagshu" --> true
  */
 
-let number = 33
+// let number = 33
 
-let stnumber = String(number)
-console.log(stnumber);
-console.log(typeof(stnumber));
+// let stnumber = String(number)
+// console.log(stnumber);
+// console.log(typeof(stnumber));
+
+// ********************************* OPERATIONS********************************* 
+
+// let val = 3
+// let negval = -val
+// console.log(negval);
+
+// let str1 = "hello"
+// let str2 = " debangshu"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"")
+
+// let num1, num2 , num3
+
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter ++;
+++ gameCounter;
+
+console.log(gameCounter);
 

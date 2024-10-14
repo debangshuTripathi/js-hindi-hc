1b6f6bd8248a0c0a3e2cb0528fe81a4d7f1175de
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-14 04:24:29 +0000

    math and num done

---
+++ 01_basics/numbers_math.js
@@ -0,0 +1,43 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.89666
+// console.log(otherNumber.toPrecision(3));
+
+
+// const otherNumber = 123.89666
+// console.log(otherNumber.toPrecision(3));
+
+// const hundrades = 1000000
+// console.log(hundrades.toLocaleString('en-IN'));
+
+
+
+// ********************* MATHS *********************
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // abs value
+// console.log(Math.round(4.3)); // round value
+// console.log(Math.ceil(4.3)); // top value
+// console.log(Math.floor(4.3)); // floor value
+// console.log(Math.min(4,9,8,5,4)); // min value
+// console.log(Math.max(4,9,8,5,4)); // max value
+
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log((Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min + 1) + min))
+

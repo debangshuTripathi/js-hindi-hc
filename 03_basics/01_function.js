7c00a88d0c9b7719a2c3776ef9dc92dbc91b8786
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-15 09:17:37 +0000

    function started

---
+++ 03_basics/01_function.js
@@ -0,0 +1,30 @@
+
+// function sayMyName() {
+//     console.log("H")
+//     console.log("I")
+//     console.log("T")
+//     console.log("E")
+//     console.log("S")
+//     console.log("H")
+// }
+// sayMyName()
+
+// function addTwoNumbers(num1,num2){ // parameters(functio decleare)
+
+//    console.log( num1+num2);
+   
+// }
+// const result = addTwoNumbers(3,5) // arguments (function calling)
+
+
+function addTwoNumbers(num1,num2){ // parameters(functio decleare)
+
+//   let result = num1 + num2
+//   return result
+
+return num1+num2
+   
+}
+const result = addTwoNumbers(3,5) // arguments (function calling)
+
+console.log("Result " ,result);

fc5cdcb4e9f1c36f39940845c301d143538226a6
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-16 09:32:28 +0000

    while and do-while done

---
+++ 05_iteration/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of idx is ${index}`);
+    index +=2
+}
+
+let myArr = ["flash","batman","superman"]
+let i = 0 
+while(i< myArr.length){
+   // console.log(`the value is ${myArr[i]}`);
+    i++
+    
+}
+
+let score = 11
+do {
+    console.log(`The score is ${score}`);
+    score++
+    
+} while (score<=10);

5b71542e892168d50d7f15252171af63378f4561
debangshuTripathi <debangshutripathi08@gmail.com>
2024-10-16 04:46:30 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+// if 
+
+// const isUserloggedIn = true
+// const temperature = 43
+// if (temperature < 50) {
+//     console.log("temp less than 50");
+    
+// }
+// else console.log("temp greater than 50");
+
+
+// < , > , <= , >= , == , != , ===
